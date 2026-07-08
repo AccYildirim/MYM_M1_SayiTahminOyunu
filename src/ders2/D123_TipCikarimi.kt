19e05950ce6570b1e7e63e78091d98fa5bf1d9a1
AhmetCan Cuma Yıldırım <ahmetcancumayildirim@gmail.com>
2026-07-08 13:14:20 +0300

    Ders2: Değişkenler ve Tipler

---
+++ src/ders2/D123_TipCikarimi.kt
@@ -0,0 +1,13 @@
+package ders2
+
+fun main() {
+    val sehir: String = "Ankara"
+    val sicaklik = 15
+    val nemOrani = 0.45
+    val dogruMu = true
+
+    var sistemMesaji: String
+    sistemMesaji = "Sıcaklık, Uyarı seviyesinde!"
+    println("Mesaj: " + sistemMesaji)
+
+}
