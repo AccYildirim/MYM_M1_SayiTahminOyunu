5d126dc6bed4392450143f39b6e8db125009c173
AhmetCan Cuma Yıldırım <ahmetcancumayildirim@gmail.com>
2026-07-16 19:56:27 +0300

    Ders5: Proje; Sayı Tahmin Oyunu

---
+++ src/ders5/Proje_SayiTahminOyunu.kt
@@ -0,0 +1,24 @@
+package ders5
+
+fun main() {
+    val hedefSayi = (1..100).random()
+
+    println("🎆🎆🎆🎆🎆---SAYI-TAHMİN-OYUNUNA-HOŞ-GELDİNİZ---🎆🎆🎆🎆🎆")
+    println("Sayı Tahminim: ")
+
+    val kullaniciTahmini = readln().toIntOrNull()
+
+    if (kullaniciTahmini == null) {
+        println("HATA: Lütfen Geçerli Bir Sayı Giriniz! 🫩")
+    } else if (kullaniciTahmini < 1 || kullaniciTahmini > 100) {
+        println("HATA: Lütfen 1 İle 100 Arasında Bir Rakam Giriniz! 😤")
+    } else if (kullaniciTahmini < hedefSayi) {
+        println("Daha Büyük Bir Sayı Denemeliydin! 😢")
+    } else if (kullaniciTahmini > hedefSayi) {
+        println("Daha Küçük Bir Sayı Denemeliydin! 😢")
+    } else {
+        println("😀😀😀🎉🎉🎉TEBRİKLER ADAMIM, SAYIYI BULDUN!!!🎉🎉🎉😀😀😀 (Hile Mi Açtın Yoksa?🤨) ")
+    }
+
+
+}
