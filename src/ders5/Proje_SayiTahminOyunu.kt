a6003215930a3560398895b25e4857efbd39dca7
AhmetCan Cuma Yıldırım <ahmetcancumayildirim@gmail.com>
2026-07-16 22:27:36 +0300

    Ders5: Proje; Sayı Tahmin Oyunu V2

--- src/ders5/Proje_SayiTahminOyunu.kt
+++ src/ders5/Proje_SayiTahminOyunu.kt
@@ -13,9 +13,9 @@ fun main() {
     } else if (kullaniciTahmini < 1 || kullaniciTahmini > 100) {
         println("HATA: Lütfen 1 İle 100 Arasında Bir Rakam Giriniz! 😤")
     } else if (kullaniciTahmini < hedefSayi) {
-        println("Daha Büyük Bir Sayı Denemeliydin! 😢")
+        println("Daha Büyük Bir Sayı Denemeliydin! 😢 (Sayı $hedefSayi idi.)")
     } else if (kullaniciTahmini > hedefSayi) {
-        println("Daha Küçük Bir Sayı Denemeliydin! 😢")
+        println("Daha Küçük Bir Sayı Denemeliydin! 😢 (Sayı $hedefSayi idi.)")
     } else {
         println("😀😀😀🎉🎉🎉TEBRİKLER ADAMIM, SAYIYI BULDUN!!!🎉🎉🎉😀😀😀 (Hile Mi Açtın Yoksa?🤨) ")
     }
