b035c81fe8cdbb0ab92d189f003362f6c388d40e
AhmetCan Cuma Yıldırım <ahmetcancumayildirim@gmail.com>
2026-07-17 13:06:33 +0300

    Ders5: Proje; Sayı Tahmin Oyunu V3

--- src/ders5/Proje_SayiTahminOyunu.kt
+++ src/ders5/Proje_SayiTahminOyunu.kt
@@ -8,17 +8,18 @@ fun main() {
 
     val kullaniciTahmini = readln().toIntOrNull()
 
-    if (kullaniciTahmini == null) {
-        println("HATA: Lütfen Geçerli Bir Sayı Giriniz! 🫩")
-    } else if (kullaniciTahmini < 1 || kullaniciTahmini > 100) {
-        println("HATA: Lütfen 1 İle 100 Arasında Bir Rakam Giriniz! 😤")
-    } else if (kullaniciTahmini < hedefSayi) {
-        println("Daha Büyük Bir Sayı Denemeliydin! 😢 (Sayı $hedefSayi idi.)")
-    } else if (kullaniciTahmini > hedefSayi) {
-        println("Daha Küçük Bir Sayı Denemeliydin! 😢 (Sayı $hedefSayi idi.)")
-    } else {
-        println("😀😀😀🎉🎉🎉TEBRİKLER ADAMIM, SAYIYI BULDUN!!!🎉🎉🎉😀😀😀 (Hile Mi Açtın Yoksa?🤨) ")
-    }
-
+    val mesaj =
+        if (kullaniciTahmini == null) {
+            "HATA: Lütfen Geçerli Bir Sayı Giriniz! 🫩"
+        } else if (kullaniciTahmini < 1 || kullaniciTahmini > 100) {
+            "HATA: Lütfen 1 İle 100 Arasında Bir Rakam Giriniz! 😤"
+        } else if (kullaniciTahmini < hedefSayi) {
+            "Daha Büyük Bir Sayı Denemeliydin! 😢 (Sayı $hedefSayi idi.)"
+        } else if (kullaniciTahmini > hedefSayi) {
+            "Daha Küçük Bir Sayı Denemeliydin! 😢 (Sayı $hedefSayi idi.)"
+        } else {
+            "😀😀😀🎉🎉🎉TEBRİKLER ADAMIM, SAYIYI BULDUN!!!🎉🎉🎉😀😀😀 (Hile Mi Açtın Yoksa?🤨)"
+        }
+    println(mesaj)
 
 }
